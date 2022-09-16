f91dedccb05ddf537f16e05ac029d3150776950a
Shedrack Sunday <Hannania002@gmail.com>
2022-09-16 03:53:23 +0100

    Add changes to tect errors and linters on home.js

--- home.js
+++ home.js
@@ -1,28 +1,27 @@
-//Menu selector
+// Menu selector
 
 const menuBtn = document.querySelector('.menu-btn');
-//mobile header selector
+// mobile header selector
 
 const headerNavMobile = document.querySelector('.header-nav-mobile');
-//close button selector
+// close button selector
 
 const btnCloseNav = document.querySelector('.btn-close-nav');
 const speakerContainerCard = document.querySelector('.speaker-container-card');
-const feature_speaker_button = document.querySelector('.Feature_speaker_button');
-//line 10 to 13 selects the nav bar details to be clicked.
+const featureSpeakerButton = document.querySelector('.Feature_speaker_button');
+// line 10 to 13 selects the nav bar details to be clicked.
 
 const linkAbout = document.querySelector('.link-about');
 const linkProgram = document.querySelector('.link-program');
 const linkJoin = document.querySelector('.link-join');
 const linkSponsor = document.querySelector('.link-sponsor');
 
-/*A media query selector to later adjust the display of speakers on the screen, at max-width of
-768, which is a mobile version of the app*/
+/* A media query selector to later adjust the display of speakers on the screen, at max-width of
+768, which is a mobile version of the app */
 
-const mediaqueryList = window.matchMedia('(max-width: 768px)');
+const mediaQueryList = window.matchMedia('(max-width: 768px)');
 
-
-//Line 19 to 47 shows the click function behaviour
+// Line 19 to 47 shows the click function behaviour
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.add('hidden');
@@ -54,14 +53,14 @@ linkSponsor.addEventListener('click', () => {
   menuBtn.classList.remove('hidden');
 });
 
-const InvitedSpeakers = [
+const invitedSpeakers = [
   {
     id: 0,
     url: './images/Elon-Musk-2010.jpeg',
     name: 'Elon Musk',
     role: 'CEO  of  SpaceX and Tesla.',
     description:
-      ' Elon is a Great Enterpreanure and richest man alive. his company has made gaint strides in space shuttle, and electric car industry.',
+      ' Elon is a Great Enterpreanure and richest man alive. his company has made gaint strides in space shuttle, and the electric car industry.',
   },
   {
     id: 1,
@@ -69,7 +68,7 @@ const InvitedSpeakers = [
     name: 'Jeff Bezoz',
     role: 'Founder of Amazon',
     description:
-      'Jeff was once the richest man in the planet, and that was as a result of his great company. AWS, a subsidary of Amazon, is a market leader in cloud computing',
+      'Jeff was once the richest man on the planet, and that was as a result of his great company AWS which is  a subsidary of Amazon. Presently  a market leader in cloud computing',
   },
   {
     id: 2,
@@ -77,7 +76,7 @@ const InvitedSpeakers = [
     name: 'Satya Nadella',
     role: 'CEO at MicroSoft inc',
     description:
-      'Nadella signature achievement as CEO has been Microsoft Wholesale aoption of cloud computing. creating the Azure public cloud business, while sifting most of te company software applications to the cloud',
+      'Nadella signature achievement as CEO has seen Microsoft Wholesale adoption of cloud computing. Creating the Azure public cloud business, while sifting most of the company software applications to the cloud',
   },
   {
     id: 3,
@@ -85,7 +84,7 @@ const InvitedSpeakers = [
     name: 'Jensen Huang',
     role: 'CEO AT NVIDIA.',
     description:
-      "Jensen helped bring life to te graphics chip industry, and has been the CEO since founding the company in 1993. Today, the company is a leader in the grraphics space",
+      'Jensen helped bring life to te graphics chip industry, and has been the CEO since founding the company in 1993. Today, the company is a leader in the grraphics space',
   },
   {
     id: 4,
@@ -93,7 +92,7 @@ const InvitedSpeakers = [
     name: 'Jim Rose',
     role: 'CEO at CircleCI',
     description:
-      "Jim is the CEO of te continuous integration and delivery platform named CircleCI, which is a major market leader in the cloud space. ",
+      'Jim is the CEO of the continuous integration and delivery platform named CircleCI, which is a major market leader in the cloud space.',
   },
   {
     id: 5,
@@ -101,27 +100,29 @@ const InvitedSpeakers = [
     name: 'Sundar Pichai',
     role: 'CEO st Alphabet Inc.',
     description:
-      'Sundar is the CEO if the biggest Tech comapny in the world. Google offers cloud computing at an accelerated rate hrough its google cloud services.',
+      'Sundar is the CEO if the biggest Tech comapny in the world. Google offers cloud computing at an accelerated rate through its google cloud services.',
   },
 ];
 
-const card_for_speakers = (index) => `
+const cardForSpeakers = (index) => `
   <div class="speaker-card-img">
-      <img src=${InvitedSpeakers[index].url} alt="image of speaker ${InvitedSpeakers[index].name}">
+      <img src=${invitedSpeakers[index].url} alt="image of speaker ${invitedSpeakers[index].name}">
   </div>
   <div class="speaker-card-content">
       <div class="speaker-card-name">
           <p>
-              ${InvitedSpeakers[index].name}
+            ${invitedSpeakers[index].name}
           </p>
       </div>
       <div class="speaker-card-position">
           <p>
-              ${InvitedSpeakers[index].role}
+            ${invitedSpeakers[index].role}
           </p>
       </div>
       <div class="speaker-card-description">
-          <p>${InvitedSpeakers[index].description}</p>
+          <p>
+          ${invitedSpeakers[index].description}
+          </p>
       </div>
   </div>
   `;
@@ -136,54 +137,52 @@ const twoSpeakers = () => {
   //enlist just two speakers when the 'MORE' Button is on screen
   for (let i = 0; i < 2; i += 1) {
     const speaker_Tag = document.createElement('div');
-    speaker_Tag.classList.add('speaker-card');
-    speaker_Tag.innerHTML = card_for_speakers(i);
-    speakerContainerCard.appendChild(speaker_Tag);
+    speakerTag.classList.add('speaker-card');
+    speakerTag.innerHTML = cardForSpeakers(i);
+    speakerContainerCard.appendChild(speakerTag);
   }
 };
 
-//This function dynamically deploy the speaker cards created frpm the 
+//This function dynamically deploy the speaker cards created from the 
 //card_for_speaker function above.
 
 const allSpeakers = () => {
   speakerContainerCard.innerHTML = ' ';
-  for (let i = 0; i < InvitedSpeakers.length; i += 1) {
+  for (let i = 0; i < invitedSpeakers.length; i += 1) {
     const speaker = document.createElement('div');
     speaker.classList.add('speaker-card');
-    speaker.innerHTML = card_for_speakers(i);
+    speaker.innerHTML = cardForSpeakers(i);
     speakerContainerCard.appendChild(speaker);
   }
 };
 
-//This function dynamically list the speakers or lessens it, based on the LESSS or MORE Button
-feature_speaker_button.addEventListener('click', () => {
+/* This function dynamically list the speakers or lessens it, based on the LESSS or MORE Button */
+
+featureSpeakerButton.addEventListener('click', () => {
   const regex = /MORE/;
-  const flag = regex.test(feature_speaker_button.textContent);
+  const flag = regex.test(featureSpeakerButton.textContent);
   if (flag) {
-    feature_speaker_button.innerHTML = 'LESS <i class="fas fa-chevron-up"></i>';
+    featureSpeakerButton.innerHTML = 'LESS <i class="fas fa-chevron-up"></i>';
     allSpeakers();
-  } 
-  else {
-    feature_speaker_button.innerHTML = 'MORE <i class="fas fa-chevron-down"></i>';
+  }else {
+    featureSpeakerButton.innerHTML = 'MORE <i class="fas fa-chevron-down"></i>';
     twoSpeakers();
   }
 });
 
-/*function decalraton to test the size of the screen.  
-it displays two speakers from the InvitedSpeakers array, if the mobile screen is shown 
-with the 'MORE' button, and shows all speakers if the 'LESS' is dispalyed.
+/* function decalraton to test the size of the screen.
+it displays two speakers from the InvitedSpeakers array,if the mobile screen is shown with the 'MORE' button, and shows all speakers if the 'LESS' is dispalyed.
 */
 
-const test_For_Screen_Size = (event) => {
+const testForScreenSize = (event) => {
   if (event.matches) {
     twoSpeakers();
-  }
-  else {
+  }else {
     allSpeakers();
   }
 };
 
 /* The Test_for _Screen_Size function is passed with the mediaquery selector function, which 
-   Actives only on mobile screens with max-width of 768px*/
+Actives only on mobile screens with max-width of 768px */
 
-test_For_Screen_Size(mediaqueryList);
+testForScreenSize(mediaQueryList);
